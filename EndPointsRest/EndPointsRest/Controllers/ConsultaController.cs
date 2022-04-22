15bad92568e58d51001d4b7f824b565c1177b259
Bruno Rodrigues <40580821+brunorodrigus@users.noreply.github.com>
2022-04-22 16:30:57 -0300

    Criação da estrutura do projeto

---
+++ EndPointsRest/EndPointsRest/Controllers/ConsultaController.cs
@@ -0,0 +1,37 @@
+﻿using EndPointsRest.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EndPointsRest.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class ConsultaController : ControllerBase
+    {
+        private static readonly string[] Nomes = new[]
+        {
+        "Rodrigo", "Lucas", "Gabriela", "Tânia", "Amy", "Jake", "Charles", "Isis"
+        };
+
+        private readonly ILogger<ConsultaController> _logger;
+
+        public ConsultaController(ILogger<ConsultaController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("api/[controller]/GetConsulta")]
+        public IEnumerable<Pessoa> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Pessoa
+            {
+                Nome = Nomes[Random.Shared.Next(Nomes.Length)],
+                Altura = Random.Shared.Next(148, 185),
+                Peso = Random.Shared.Next(50, 90),
+            })
+            .ToArray();
+        }
+
+    }
+}
---
+++ EndPointsRest/EndPointsRest/Models/Pessoa.cs
@@ -0,0 +1,10 @@
+﻿namespace EndPointsRest.Models
+{
+    public class Pessoa
+    {
+        public string? Nome { get; set; }
+        public int Altura { get; set; }
+        public Decimal Peso { get; set; }
+
+    }
+}
